56ec2538c504e72e01111324bd74f2c836f7ba4e
Doreen Hakimi <doreen.hakimi@gmail.com>
2013-11-15 15:36:36 -0800

    bankmanager started

---
+++ src/simcity/Bank/BankTellerRole.java
@@ -0,0 +1,20 @@
+package simcity.Bank;
+
+import simcity.PersonAgent;
+import simcity.interfaces.BankTeller;
+import agent.Role;
+
+public class BankTellerRole extends Role implements BankTeller {
+
+	protected BankTellerRole(PersonAgent p) {
+		super(p);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
