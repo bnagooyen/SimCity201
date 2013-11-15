c538e8d20212eaf0a49b89f29d3ae5f19b0ad5ab
Kimberly Santiago <kasantia@usc.edu>
2013-11-15 12:58:20 -0800

    updated marketcustomer and made foodorder and inventoryboy files

---
+++ src/market/FoodOrder.java
@@ -0,0 +1,10 @@
+package market;
+
+public class FoodOrder {
+	String type;
+	int amount;
+	public FoodOrder(String t, int a) {
+		type = t;
+		amount = a;
+	}
+}
--- src/market/MarketCustomerRole.java
+++ src/market/MarketCustomerRole.java
@@ -1,5 +1,76 @@
 package market;
 
-public class MarketCustomerRole {
+import java.util.List;
+
+import simcity.PersonAgent;
+import agent.Role;
+
+public class MarketCustomerRole extends Role {
+
+	private List<FoodOrder> order;
+	private double myCheck;
+	
+	public enum customerState { timeToOrder, waiting, paying, done, storeClosed, pending }
+	private customerState state;
+	
+	private MarketCashier mc;
+	private MarketManager manager;
+	
+	public MarketCustomerRole(PersonAgent p) {
+		super(p); 
+	}
+	
+	// messages
+	
+	public void msgGoToCashier(MarketCashier c) {
+		mc = c;
+		state = customerState.timeToOrder;	
+		stateChanged();
+	}
+	
+	public void msgHereIsOrderAndCheck(List<FoodOrder> canGive, double check) {
+		myCheck = calculateBill(canGive);
+		updateMyFood(canGive);
+		state = customerState.paying;
+		stateChanged();
+	}
+
+	public void msgMarketClosed() {
+		state = customerState.storeClosed;
+		stateChanged();
+	}
+	
+	//scheduler
+	public boolean pickAndExecuteAnAction() {
+		if ( state == customerState.timeToOrder ) {
+			orderFood();
+			return true;
+		}
+		if ( state == customerState.paying ) {
+			
+		}
+		return false;
+	}
+
+	
+	// actions
+
+	private void orderFood() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+	// utilities
+	private void updateMyFood(List<FoodOrder> canGive) {
+	// TODO Auto-generated method stub
+	
+	}
+	
+	private double calculateBill(List<FoodOrder> canGive) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
