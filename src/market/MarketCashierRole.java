89b1455637cef39ab0af56196fe1599d68d1e7e1
Linda Char <lindamch@usc.edu>
2013-11-15 13:02:00 -0800

    Starting market cashier.

---
+++ src/market/MarketCashierRole.java
@@ -0,0 +1,26 @@
+package market;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class MarketCashierRole {
+	List<Order> orders =Collections.synchronizedList(new ArrayList<Order>());
+	
+	boolean active;
+	double marketMoney;
+	
+	Person p;
+	InventoryBoy ib;
+	MarketManager manager; 
+	
+	enum orderState{pending, inquiring, ready, given, paid};
+	enum myState{arrived, working, goHome, unavailable};
+	
+	//Utilities
+	
+	class Order{
+			List<FoodOrder>foodsNeeded = Collections.synchronizedList
+	}
+}
