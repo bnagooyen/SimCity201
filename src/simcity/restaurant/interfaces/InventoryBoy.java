abbbedee1232c84f6e89ceb111725f51619524ec
Kimberly Santiago <kasantia@usc.edu>
2013-11-15 16:55:54 -0800

    updated market interfaces again

--- src/simcity/restaurant/interfaces/InventoryBoy.java
+++ src/simcity/restaurant/interfaces/InventoryBoy.java
@@ -1,5 +1,9 @@
 package simcity.restaurant.interfaces;
 
+import simcity.Market.MOrder;
+
 public interface InventoryBoy {
+	public abstract void msgCheckInventory(MOrder o);
+	public abstract void msgGoHome();
 
 }
--- src/simcity/restaurant/interfaces/MarketCashier.java
+++ src/simcity/restaurant/interfaces/MarketCashier.java
@@ -5,9 +5,13 @@ import java.util.List;
 import agent.Role;
 import simcity.Market.MFoodOrder;
 import simcity.Market.MOrder;
+import simcity.restaurant.CashierRole;
 
 public interface MarketCashier {
-	public abstract void msgOrder(Role r, List<MFoodOrder> order, String s);
+	public abstract void msgOrder(Role r, List<MFoodOrder> foods, String building);
+	public abstract void msgOrder(Role r, List<MFoodOrder> foods, String building, CashierRole c);
 	public abstract void msgCanGive(MOrder o);
 	public abstract void msgHereIsPayment(Role r, double payment);
+	public abstract void msgGoHome();
+
 }
