e99ab016b0534fac510bffd176baf6186e77bd42
Doreen Hakimi <doreen.hakimi@gmail.com>
2013-11-15 17:59:13 -0800

    bank manager scheduler finished.. on to actions

--- src/simcity/Bank/BankManagerRole.java
+++ src/simcity/Bank/BankManagerRole.java
@@ -7,8 +7,8 @@ import java.util.List;
 import java.util.Map;
 
 import simcity.PersonAgent;
-import simcity.Bank.BankManagerRole.MyEmployee.EmployeeType;
-import simcity.Bank.BankManagerRole.MyEmployee.MyTellerState;
+import simcity.Bank.BankManagerRole.MyLoanOfficer.MyOfficerState;
+import simcity.Bank.BankManagerRole.MyTeller.MyTellerState;
 import simcity.interfaces.BankLoanOfficer;
 import simcity.interfaces.BankManager;
 import simcity.interfaces.BankTeller;
@@ -17,9 +17,12 @@ import agent.Role;
 public class BankManagerRole extends Role implements BankManager {
 	
 	//data
-	List<MyEmployee> workers = Collections.synchronizedList(new ArrayList<MyEmployee>());
-	List<MyCustomer> customers = Collections.synchronizedList(new ArrayList<MyCustomer>());
-	Map<Integer, MyAccount> accounts = new HashMap<Integer, MyAccount>();
+	private List<MyTeller> tellers = Collections.synchronizedList(new ArrayList<MyTeller>());
+	private List<MyLoanOfficer> officers = Collections.synchronizedList(new ArrayList<MyLoanOfficer>());
+	private List<MyCustomer> customers = Collections.synchronizedList(new ArrayList<MyCustomer>());
+	private Map<Integer, MyAccount> accounts = new HashMap<Integer, MyAccount>();
+	int hour;
+	private double vault = 1000000000;
 	
 	protected BankManagerRole(PersonAgent p) {
 		super(p);
@@ -28,14 +31,20 @@ public class BankManagerRole extends Role implements BankManager {
 
 	
 	//MESSAGES
+	
+	public void msgTimeUpdate(int hr) {
+		hour=hr;
+		stateChanged();
+	}
+	
 	public void msgIAmHere(Role person, String type) {
 		//if(person.instanceof(BankTeller))
 		if(type.equals("BankTeller")) {
-			workers.add(new MyEmployee(person, MyEmployee.EmployeeType.Teller));
+			tellers.add(new MyTeller((BankTeller)person));
 			stateChanged();
 		}
 		else if (type.equals("BankLoanOfficer")) {
-			workers.add(new MyEmployee(person, MyEmployee.EmployeeType.LoanOfficer));
+			officers.add(new MyLoanOfficer((BankLoanOfficer)person));
 			stateChanged();
 		}
 		else if(type.equals("loan")) {
@@ -49,73 +58,123 @@ public class BankManagerRole extends Role implements BankManager {
 	}
 	
 	public void msgAvailable(BankTeller t) {
-		for (MyEmployee wee: workers) {
-			if(wee.type==EmployeeType.Teller) {
-				wee.state=MyTellerState.available;
-				stateChanged();
-			}
-		}
+		tellers.get(0).state=MyTeller.MyTellerState.available;
+		stateChanged();	
 	}
 
 	public void msgAvailable(BankLoanOfficer t) {
-		for (MyEmployee wee: workers) {
-			if(wee.type==EmployeeType.Teller) {
-				wee.state=MyTellerState.available;
-				stateChanged();
-			}
-		}
+		officers.get(0).state=MyLoanOfficer.MyOfficerState.available;
+		stateChanged();		
 	}
 	
 	public void msgCreateAccount(String type) {
 		if(type.equals("BankTeller")) {
-			for(MyEmployee e: workers) {
-				if(e.type==EmployeeType.Teller) {
-					e.needsAccount=true;
-					stateChanged();
-				}
-			}
+			tellers.get(0).needsAccount=true;
+			stateChanged();	
 		}
 		
 		if(type.equals("LoanAgent")) {
-			for(MyEmployee e: workers) {
-				if(e.type==EmployeeType.LoanOfficer) {
-					e.needsAccount=true;
-					stateChanged();
-				}
-			}
+			officers.get(0).needsAccount=true;
+			stateChanged();
 		}
 	}
-	
 	public void msgProcessTransaction(int AN, double amount) {
-		for(MyEmployee e: workers) {
-			if(e.type==EmployeeType.Teller) {
-				e.requested=amount;
-				e.accountNum=AN;
-				stateChanged();
-			}
-		}
+		tellers.get(0).requested=amount;
+		tellers.get(0).accountNum=AN;
+		stateChanged();
+	}
+	
+	public void msgNewLoan(int AN, double amount) {
+		officers.get(0).requested=amount;
+		officers.get(0).accountNum=AN;
+		stateChanged();
+	}
+	
+	public void msgGaveALoan(double cash) {
+		vault -= cash;
+		stateChanged();
 	}
 
 	@Override
 	public boolean pickAndExecuteAnAction() {
 		// TODO Auto-generated method stub
+		
+		if(tellers.size()>1) {
+			SwapTellers();
+			return true;
+		}
+		
+		if(officers.size()>1) {
+			SwapLoanOfficers();
+			return true;
+		}
+		if(officers.size()==1 && officers.get(0).state==MyOfficerState.justArrived) {
+			AddLoanOfficer();
+			return true;
+		}
+		
+		if(tellers.size()==1 && tellers.get(0).state==MyTellerState.justArrived) {
+			AddTeller();
+			return true;
+		}
+		
+		if(tellers.get(0).needsAccount) {
+			NewAccount();
+			return true;
+		}
+		
+		if(tellers.get(0).requested!=0) {
+			CompleteTransaction();
+			return true;
+		}
+		
+		if(officers.get(0).requested!=0) {
+			CompleteLoan();
+			return true;
+		}
+		
+		if(hour>19) {
+			CloseBank();
+			return true;
+		}
+		
+		
 		return false;
 	}
 	
-	static class MyEmployee {
-		Role emp;
+	//actions
+	private void SwapTellers() {
+//		tellers.get(0).emp.
+	}
+	
+	static class MyTeller {
+		BankTeller emp;
 		boolean needsAccount=false;
 		int accountNum = 0;
 		double requested=0.0;
 		enum MyTellerState {justArrived, available, occupied};
 		MyTellerState state;
+		
+		MyTeller (BankTeller p) {
+			emp = p;
+			state= MyTellerState.justArrived;
+		}
+		
+	}
+
+	static class MyLoanOfficer {
+		BankLoanOfficer emp;
+		boolean needsAccount=false;
+		int accountNum = 0;
+		double requested=0.0;
+		enum MyOfficerState {justArrived, available, occupied};
+		MyOfficerState state;
 		enum EmployeeType {LoanOfficer, Teller};
 		EmployeeType type;
 		
-		MyEmployee (Role p, EmployeeType tp) {
-			type=tp;
+		MyLoanOfficer (BankLoanOfficer p) {
 			emp = p;
-			state= MyTellerState.justArrived;
+			state= MyOfficerState.justArrived;
 		}
 		
 	}
