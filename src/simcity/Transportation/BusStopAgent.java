94dbeed2b7030bd6aa4081acc8063ff49def1d77
Brian Nguyen <briannguyen3@yahoo.com>
2013-11-15 16:36:07 -0800

    add bus stop

---
+++ src/simcity/Transportation/BusStopAgent.java
@@ -0,0 +1,53 @@
+package simcity.Transportation;
+
+
+import simcity.PersonAgent;
+import java.util.*;
+import agent.Agent;
+import simcity.Transportation.BusAgent;
+
+
+public class BusStopAgent extends Agent{
+	
+	List<BusAgent> buses;
+	List<PersonAgent> waitingPassengers;
+	
+	
+	//Messages
+	
+	public void msgWaitingForBus(PersonAgent p){
+		waitingPassengers.add(p);
+		stateChanged();
+	}
+	
+	public void msgAnyPassengers(BusAgent b){
+		buses.add(b);
+		stateChanged();
+	}
+	
+	
+	//ACTIONS
+	
+	private void givePassengerList(BusAgent b){
+		b.msgHereArePassegers(waitingPassengers);
+		waitingPassengers.clear();
+		buses.remove(b);
+	}
+	
+	
+	
+	
+	
+	
+	//SCHEDULER
+	
+	protected boolean pickAndExecuteAnAction(){
+	
+		if(buses.size()>0){
+			//givePassengerList(buses.get(0))
+		}
+		
+		return false;
+	}
+	
+}
