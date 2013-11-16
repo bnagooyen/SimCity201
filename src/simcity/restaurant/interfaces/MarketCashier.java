823e622390b0a0394e4d5fa3c672c45095aeb6d3
Kimberly Santiago <kasantia@usc.edu>
2013-11-15 16:51:08 -0800

    updated market interfaces

--- src/simcity/restaurant/interfaces/MarketCashier.java
+++ src/simcity/restaurant/interfaces/MarketCashier.java
@@ -1,5 +1,13 @@
 package simcity.restaurant.interfaces;
 
-public interface MarketCashier {
+import java.util.List;
 
+import agent.Role;
+import simcity.Market.MFoodOrder;
+import simcity.Market.MOrder;
+
+public interface MarketCashier {
+	public abstract void msgOrder(Role r, List<MFoodOrder> order, String s);
+	public abstract void msgCanGive(MOrder o);
+	public abstract void msgHereIsPayment(Role r, double payment);
 }
--- src/simcity/restaurant/interfaces/MarketManager.java
+++ src/simcity/restaurant/interfaces/MarketManager.java
@@ -1,5 +1,8 @@
 package simcity.restaurant.interfaces;
 
+import agent.Role;
+
 public interface MarketManager {
 
+	public abstract void msgIAmHere(Role r, String s);
 }
