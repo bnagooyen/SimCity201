1492db8d6d8f918f2bb4cfed3f7f9032d6bbbb0a
Drew Appleman <Aapplema@usc.edu>
2013-11-17 20:05:41 -0800

    Added Bank MockAgents and BankTeller Testing

---
+++ src/simcity/test/mock/MockBankCustomer.java
@@ -0,0 +1,36 @@
+package simcity.test.mock;
+
+import simcity.interfaces.*;
+
+public class MockBankCustomer extends Mock implements BankCustomer{
+
+	EventLog log;
+	public MockBankCustomer(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	public void msgGoToLoanOfficer(BankLoanOfficer BL){
+		
+	}
+	
+	public void msgGoToTeller(BankTeller BT){
+		
+	}
+	
+	public void msgTransactionComplete(double amount){
+		
+	}
+	
+	public void msgHeresLoan(double amount){
+		
+	}
+	
+	public void msgLoanDenied(){
+		
+	}
+	
+	public void msgAccountMade(int AN){
+		
+	}
+
+}
