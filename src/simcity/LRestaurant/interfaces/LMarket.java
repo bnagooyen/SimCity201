e1cfb8f21a73a39f2aeb6a583da8400ed2196250
Linda Char <lindamch@usc.edu>
2013-11-21 17:33:47 -0800

    Fixed errors having to do with market.

--- src/simcity/LRestaurant/interfaces/LMarket.java
+++
@@ -1,9 +0,0 @@
-package simcity.LRestaurant.interfaces;
-
-public interface LMarket {
-	
-	public abstract void msgNeedFood(int amount, String choice);
-	
-	public abstract void msgHereIsMoney(int amount);
-
-}
