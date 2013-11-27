b2bfe07dde5707f79da717e61c7822d251d0b4df
Brian Nguyen <briannguyen3@yahoo.com>
2013-11-26 22:36:20 -0800

    edits again

--- src/simcity/Transportation/BusStopAgent.java
+++ src/simcity/Transportation/BusStopAgent.java
@@ -13,47 +13,47 @@ import simcity.interfaces.Person;
 
 
 public class BusStopAgent extends Agent implements BusStop{
-	
-	public List<Bus> buses=new ArrayList<Bus>();
-	public List<PersonAgent> waitingPassengers=new ArrayList<PersonAgent>();
-	public String myStop;
-	
-	
-	//Messages
-	
-	public void msgWaitingForBus(PersonAgent p){
-		waitingPassengers.add(p);
-		stateChanged();
-	}
-	
-	public void msgAnyPassengers(Bus b){
-		
-		buses.add(b);
-		stateChanged();
-	}
-	
-	
-	//SCHEDULER
-	
-	public boolean pickAndExecuteAnAction(){
-	
-		if(buses.size()>0){
-			givePassengerList(buses.get(0));
-			
-			return true;
-		}
-		
-		return false;
-	}
-	
-	
-	//ACTIONS
-	
-	private void givePassengerList(Bus bus){
-		bus.msgHereArePassengers(waitingPassengers);
-		waitingPassengers.clear();
-		buses.remove(bus);
-	}
-	
-	
+        
+        public List<Bus> buses=new ArrayList<Bus>();
+        public List<PersonAgent> waitingPassengers=new ArrayList<PersonAgent>();
+        public String myStop;
+        
+        
+        //Messages
+        
+        public void msgWaitingForBus(PersonAgent p){
+                waitingPassengers.add(p);
+                stateChanged();
+        }
+        
+        public void msgAnyPassengers(Bus b){
+                
+                buses.add(b);
+                stateChanged();
+        }
+        
+        
+        //SCHEDULER
+        
+        public boolean pickAndExecuteAnAction(){
+        
+                if(buses.size()>0){
+                        givePassengerList(buses.get(0));
+                        
+                        return true;
+                }
+                
+                return false;
+        }
+        
+        
+        //ACTIONS
+        
+        private void givePassengerList(Bus bus){
+                bus.msgHereArePassengers(waitingPassengers);
+                waitingPassengers.clear();
+                buses.remove(bus);
+        }
+        
+        
 }
