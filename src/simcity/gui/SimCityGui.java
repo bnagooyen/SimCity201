e548a0ae261153ace1f4449a3818d2278f7a462f
Doreen Hakimi <doreen.hakimi@gmail.com>
2013-12-08 20:35:40 -0800

    adding file i/o again

--- src/simcity/gui/SimCityGui.java
+++ src/simcity/gui/SimCityGui.java
@@ -6,6 +6,7 @@ import simcity.gui.BuildingListPanel;
 import simcity.gui.trace.AlertLevel;
 import simcity.gui.trace.AlertTag;
 import simcity.gui.trace.TracePanel;
+import simcity.interfaces.Person;
 
 import javax.swing.*;
 
@@ -14,6 +15,7 @@ import simcity.DRestaurant.DWaiterRole;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 /**
  * Main GUI class.
@@ -28,7 +30,7 @@ public class SimCityGui extends JFrame implements ActionListener {
 	
 	//AnimationPanel animationPanel = new AnimationPanel(this);
 	//SimCityAnimationPanel simCityPanel = new SimCityAnimationPanel(this);
-	ListPanel addPersonPanel = null;
+	//ListPanel addPersonPanel = null;
 	
 	CityPanel city;
 	BuildingListPanel info;
@@ -88,151 +90,6 @@ public class SimCityGui extends JFrame implements ActionListener {
         int WINDOWY = 750;
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-//         simCityFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//         simCityFrame.setBounds(50, 25 , WINDOWX+275, 462);
-//         simCityFrame.setVisible(true);
-//         simCityFrame.setLayout(new BorderLayout());
-////     	animationFrame.add(animationPanel); 
-//         simCityFrame.add(simCityPanel, BorderLayout.CENTER);
-//         addPersonPanel=new ListPanel(restPanel, "Person");
-//         
-//         JPanel cityPanels = new JPanel();
-//         cityPanels.setLayout(new GridLayout(1,1));
-//         cityPanels.setMaximumSize(new Dimension((int)(WINDOWX*0.4), (200)));
-//         cityPanels.setPreferredSize(new Dimension((int)(WINDOWX*0.4), (200)));
-//         cityPanels.setMinimumSize(new Dimension((int)(WINDOWX*0.4), (200)));
-//         cityPanels.add(addPersonPanel);
-//         
-//         simCityFrame.add(cityPanels, BorderLayout.WEST);
-//         
-//         simCityFrame.validate();
-        
-       // animationFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       // animationFrame.setBounds(100+WINDOWX, 50 , WINDOWX+100, WINDOWY+100);
-       // animationFrame.setVisible(true);
-    	//animationFrame.add(animationPanel); 
-//        animationPanel.setBounds(100+WINDOWX, 50 , WINDOWX+100, WINDOWY+100);
-//    	
-//    	setBounds(50, 50, WINDOWX, WINDOWY);
-//
-//        setLayout(new BorderLayout());
-//    	
-//    	//setLayout(new FlowLayout(FlowLayout.LEFT, 5,5));
-//
-//    	
-//        Dimension restDim = new Dimension(WINDOWX, (WINDOWY));
-//        
-//        JPanel utilitiesPanel= new JPanel();
-//        utilitiesPanel.setLayout(new BorderLayout());
-//        
-//        //restPanel.setPreferredSize(restDim);
-//        restPanel.setMinimumSize(restDim);
-//        //restPanel.setMaximumSize(restDim);
-//        utilitiesPanel.add(restPanel, BorderLayout.CENTER);
-//        
-//    
-//        
-//        //JPanel utSubPanel = new JPanel();
-//        //utSubPanel.setLayout(new GridLayout(2,1));
-//        
-//        // Now, setup the info panel
-//        Dimension infoDim = new Dimension(WINDOWX*(int)0.3, (int) (WINDOWY * .02));
-//        infoPanel = new JPanel();
-////        infoPanel.setLayout(new BorderLayout());
-//        //infoPanel.setPreferredSize(infoDim);
-//        //infoPanel.setMinimumSize(infoDim);
-//        //infoPanel.setMaximumSize(infoDim);
-//        infoPanel.setBorder(BorderFactory.createTitledBorder("Information"));
-//    
-//        //stateCB = new JCheckBox();
-//        //stateCB.setVisible(false);
-//        //stateCB.addActionListener(this);
-//
-//        infoPanel.setLayout(new BorderLayout());
-//        
-//        infoLabel = new JLabel();
-//        infoLabel.setText("How to add customers: (Name), (WalletAmount), (Choice), (\"Stay\"/\"Leave\")    |    Example: Doreen, 23.55, Steak, Leave");
-//        infoPanel.add(infoLabel, BorderLayout.NORTH);
-//        //infoPanel.add(stateCB);
-//        
-//        pause = new JButton();
-//        pause.setText("Pause!");
-//        pause.addActionListener(this);
-//        infoPanel.add(pause, BorderLayout.EAST);
-//        
-//        JPanel updateValsPanel = new JPanel();
-//        updateValsPanel.setLayout(new GridLayout(3,3));
-//  
-//        private JButton kitchenThresholdInc;
-//        private JButton kitchenThresholdDec;
-//        private JButton kitchenAmntInc;
-//        private JButton kitchenAmntDec;
-//        private JButton marketAmntInc;
-//        private JButton marketAmntDec;
-        
-//        updateValsPanel.add(new JLabel("Update Kitchen Threshold"));
-//        kitchenThresholdInc= new JButton("+");
-//        kitchenThresholdInc.addActionListener(this);
-//        updateValsPanel.add(kitchenThresholdInc);
-//        kitchenThresholdDec= new JButton("-");
-//        kitchenThresholdDec.addActionListener(this);
-//        updateValsPanel.add(kitchenThresholdDec);
-//        
-//        updateValsPanel.add(new JLabel("Update Kitchen Amount"));
-//        kitchenAmntInc= new JButton("+");
-//        kitchenAmntInc.addActionListener(this);
-//        updateValsPanel.add(kitchenAmntInc);
-//        kitchenAmntDec= new JButton("-");
-//        kitchenAmntDec.addActionListener(this);
-//        updateValsPanel.add(kitchenAmntDec);
-//        
-//        updateValsPanel.add(new JLabel("Update Market Amount"));
-//        marketAmntInc= new JButton("+");
-//        marketAmntInc.addActionListener(this);
-//        updateValsPanel.add(marketAmntInc);
-//        marketAmntDec= new JButton("-");
-//        marketAmntDec.addActionListener(this);
-//        updateValsPanel.add(marketAmntDec);
-//        
-//        infoPanel.add(updateValsPanel, BorderLayout.WEST);
-        
-//        setAmounts= new JButton("SET");
-//        setAmounts.addActionListener(this);
-//        infoPanel.add(setAmounts,BorderLayout.CENTER);
-//        
-        
-        
-        
-        
-        // add(infoPanel, BorderLayout.CENTER);
-        
-        //utilitiesPanel.add(utSubPanel);
-//        utilitiesPanel.add(infoPanel, BorderLayout.SOUTH);
-//        add(utilitiesPanel, BorderLayout.NORTH);
-//        
-//        add(animationPanel, BorderLayout.CENTER);
-//        
-        /*
-        
-        */
-        //my panel
-        /* lab1
-         
-        
-        addingID = new JPanel();
-        addingID.setPreferredSize(infoDim);
-        addingID.setMinimumSize(infoDim);
-        addingID.setMaximumSize(infoDim);
-        addingID.setBorder(BorderFactory.createTitledBorder("MyInfo"));
-
-        addingID.setLayout(new GridLayout(1, 2, 30, 0));
-        
-        myID = new JLabel(); 
-        myID.setText("Doreen Hakimi!");
-        addingID.add(myID);
-        add(addingID);
-	*/
         
         setBounds(20, 0, 875, 444);
 		tracePanel = new TracePanel();
@@ -575,77 +432,14 @@ public class SimCityGui extends JFrame implements ActionListener {
                 stateCB.setEnabled(false);
             }
         }
-//        if(e.getSource()== pause) {
-//        	restPanel.msgTogglePause();
-//        }
-//        if(e.getSource()==)
-//      private JButton kitchenThresholdInc;
-//      private JButton kitchenThresholdDec;
-//      private JButton kitchenAmntInc;
-//      private JButton kitchenAmntDec;
-//      private JButton marketAmntInc;
-//      private JButton marketAmntDec;
-//        if(e.getSource()==kitchenThresholdInc) {
-//        	restPanel.msgIncreaseKitchenThreshold();
-//        }
-//        if(e.getSource()==kitchenThresholdDec) {
-//        	restPanel.msgDecreaseKitchenThreshold();
-//        }
-//        if(e.getSource()==kitchenAmntInc) {
-//        	restPanel.msgIncreaseKitchenAmount();
-//        }
-//        if(e.getSource()==kitchenAmntDec) {
-//        	restPanel.msgDecreaseKitchenAmount();
-//        }
-//        if(e.getSource()==marketAmntInc) {
-//        	restPanel.msgIncreaseMarketAmount();
-//        }
-//        if(e.getSource()==marketAmntDec) {
-//        	restPanel.msgDecreaseMarketAmount();
-//        }
-//        
-//        if(e.getSource()==setAmounts) {
-//        	kitchenThresholdInc.setEnabled(false);
-//        	kitchenThresholdDec.setEnabled(false);
-//        	kitchenAmntDec.setEnabled(false);
-//        	kitchenAmntInc.setEnabled(false);
-//        	marketAmntDec.setEnabled(false);
-//        	marketAmntInc.setEnabled(false);
-//        	setAmounts.setEnabled(false);
-//        	restPanel.msgInventoryValsSet();
-//        }
-//        
+       
         
     }
-    /**
-     * Message sent from a customer gui to enable that customer's
-     * "I'm hungry" checkbox.
-     *
-     * @param c reference to the customer
-//     */
-//    public void setCustomerEnabled(CustomerRole c) {
-//       /* if (currentPerson instanceof CustomerAgent) {
-//            CustomerAgent cust = (CustomerAgent) currentPerson;
-//            if (c.equals(cust)) {
-//                stateCB.setEnabled(true);
-//                stateCB.setSelected(false);
-//            }
-//        }*/
-//    		restPanel.setCustomerEnabled(c.getText(), c.getWallet());
-//    		
-//    }
-//    
-//    public void setWaiterEnabled(WaiterRole w) {
-//    	restPanel.setWaiterEnabled(w.getName());
-//    }
-//    
-//    public void setWaiterDisabled(WaiterRole w) {
-//    	restPanel.setWaiterDisabled(w.getName());
-//    }
-//    
-//    public void setWaiterToBreak(WaiterRole w) {
-//    	restPanel.setWaiterToBreak(w.getName());
-//    }
+
+    public void AddPeople(ArrayList<Person> p) {
+    	cityInfo.AddPeople(p);
+    }
+
     /**
      * Main routine to get gui started
      */
